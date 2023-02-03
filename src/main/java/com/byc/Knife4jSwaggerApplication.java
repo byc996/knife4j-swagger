6711a69e17221bf3578fc4bbaac37619a6fd5919
Buyi Cheng <bc2996@columbia.edu>
2023-02-03 14:05:42 -0500

    init

---
+++ src/main/java/com/byc/Knife4jSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.byc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knife4jSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knife4jSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byc/Knife4jSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knife4jSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
